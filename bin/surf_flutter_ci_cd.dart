6e16cb77207c4ebbdc25c5bf51cf1e5dab44cbb6
Dmitry Shevchenko <dshevchenkoo@gmail.com>
2023-02-23 16:15:48 +0300

    init commit

---
+++ bin/surf_flutter_ci_cd.dart
@@ -0,0 +1,5 @@
+import 'package:surf_flutter_ci_cd/surf_flutter_ci_cd.dart' as surf_flutter_ci_cd;
+
+void main(List<String> arguments) {
+  print('Hello world: ${surf_flutter_ci_cd.calculate()}!');
+}
---
+++ test/surf_flutter_ci_cd_test.dart
@@ -0,0 +1,8 @@
+import 'package:surf_flutter_ci_cd/surf_flutter_ci_cd.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
