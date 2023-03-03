97dd73c322146ef08e03227930a875a142fa2002
Dmitry Shevchenko <dshevchenkoo@gmail.com>
2023-03-03 15:37:16 +0300

    Update surf_flutter_ci_cd.dart

--- bin/surf_flutter_ci_cd.dart
+++ bin/surf_flutter_ci_cd.dart
@@ -1,5 +1,38 @@
-import 'package:surf_flutter_ci_cd/surf_flutter_ci_cd.dart' as surf_flutter_ci_cd;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${surf_flutter_ci_cd.calculate()}!');
+  var parser = ArgParser();
+  parser.addOption('env', abbr: 'e', help: 'Environment name');
+  parser.addOption('proj', abbr: 'p', help: 'Project name');
+  parser.addOption('platform', abbr: 'plat', help: 'Target platform');
+
+  var results = parser.parse(arguments);
+  var env = results['env'];
+  var proj = results['proj'];
+  var platform = results['platform'];
+
+  if (arguments.isEmpty || arguments.contains('-h') || arguments.contains('--help')) {
+    print('Usage: dart script.dart [build|deploy] --env=<environment> --proj=<project> --platform=<target platform>');
+    print(parser.usage);
+    exit(0);
+  }
+
+  if (env == null || proj == null || platform == null) {
+    print('Missing arguments. Usage: dart script.dart [build|deploy] --env=<environment> --proj=<project> --platform=<target platform>');
+    exit(1);
+  }
+
+  switch (results.rest.length > 0 ? results.rest[0] : '') {
+    case 'build':
+      print('Building $proj for $platform in $env environment');
+      // execute build command here
+      break;
+    case 'deploy':
+      print('Deploying $proj for $platform to $env environment');
+      // execute deploy command here
+      break;
+    default:
+      print('Invalid command. Usage: dart script.dart [build|deploy] --env=<environment> --proj=<project> --platform=<target platform>');
+      exit(1);
+  }
 }
