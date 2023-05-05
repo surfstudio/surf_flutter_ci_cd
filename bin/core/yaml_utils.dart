db1301ee7ba4c12a8a261d390e7d23999e0e84c9
Danil Galimzyanov <86564275+GalimzyanovDanil@users.noreply.github.com>
2023-05-05 16:28:20 +0300

    fix: hot fix type casting YamlMap to Map

--- bin/core/yaml_utils.dart
+++ bin/core/yaml_utils.dart
@@ -2,8 +2,8 @@ import 'dart:io';
 
 import 'package:yaml/yaml.dart';
 
-Future<Map> readYamlConfig() async {
+Future<YamlMap> readYamlConfig() async {
   final yamlContent = await File('cd.yaml').readAsString();
-  final config = loadYaml(yamlContent) as Map;
+  final config = loadYaml(yamlContent) as YamlMap;
   return config;
 }
